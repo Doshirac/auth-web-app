5e3f03e30f41f3510f4ad39b8e0054c51f2182d1
Doshirack <kdasha09112005@gmail.com>
2025-01-27 03:00:32 +0200

    Chang port for server and client

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const authMiddleware = require("./middlewares/authMiddleware");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", authMiddleware, userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on http://localhost:${SERVER_PORT}`);
 });
